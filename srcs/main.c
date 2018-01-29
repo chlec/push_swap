fbe20b6f1603c543d82e3e2d8b53463f820fd769
Charles LECALIER <clecalie@student.42.fr>
2018-01-29 09:09:06 +0100

    ajout du quicksort

--- srcs/main.c
+++ srcs/main.c
@@ -13,10 +13,79 @@
 #include "checker.h"
 #include "push_swap.h"
 
+void exchange(int *x,int *y)
+{
+    int temp;
+
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+ 
+int choose_pivot(int i,int j )
+{
+    return ((i+j) /2);
+}
+ 
+void quicksort(int *list, int m, int n)
+{
+    int key;
+    int i;
+    int j;
+    int	k;
+
+    printf("Quicksort function called with params %d and %d\n", m, n);
+    if( m < n)
+    {
+        k = choose_pivot(m,n);
+        if (m != k)
+        {
+	        printf("On echange index m: %d (contenu %d) avec index k: %d (contenu %d)\n", m, list[m], k, list[k]);
+	        exchange(&list[m],&list[k]);
+    	}
+        key = list[m];
+        i = m + 1;
+        j = n;
+        while(i <= j)
+        {
+            while((i <= n) && (list[i] <= key))
+                i++;
+            while((j >= m) && (list[j] > key))
+                j--;
+            if( i < j && i != j)
+            {
+            	printf("On echange index i: %d (contenu %d) avec index j: %d (contenu %d)\n", i, list[i], j, list[j]);
+                exchange(&list[i],&list[j]);
+            }
+        }
+        /* swap two elements */
+        if (m != j)
+        {
+	        printf("On echange index m: %d (contenu %d) avec index j: %d (contenu %d)\n", m, list[m], j, list[j]);
+	        exchange(&list[m],&list[j]);
+    	}
+ 
+        /* recursively sort the lesser list */
+        quicksort(list,m,j-1);
+        quicksort(list,j+1,n);
+    }
+}
+void display(int *list, const int n)
+{
+    int i;
+    for(i=0; i<n; i++)
+        printf("%d\t",list[i]);
+    printf("\n");
+}
+
 void	resolve(t_pile *a, t_pile *b)
 {
-	(void)a;
 	(void)b;
+	printf("Avant:\n");
+	display(a->num, a->len);
+	quicksort(a->num, 0, a->len - 1);
+	printf("Apres:\n");
+	display(a->num, a->len);
 }
 
 int		main(int argc, char **argv)
